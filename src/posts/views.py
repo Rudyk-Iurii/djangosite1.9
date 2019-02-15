80353d9e8f05a6627e28631faf81d79fa33865a8
ironCow <rudyk.iurii@gmail.com>
2019-02-16 00:23:38 +0200

    draft + future post acces (model manager)

--- src/posts/views.py
+++ src/posts/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.utils import timezone
 from .forms import PostForm
 from .models import Post
 
@@ -29,9 +29,12 @@ def post_create(request):
     return render(request, "post_form.html", context)
 
 def post_detail(request, slug=None):
-    if not request.user.is_staff or not request.user.is_superuser:
-        raise Http404
+    # if not request.user.is_staff or not request.user.is_superuser:
+    #     raise Http404
     instance = get_object_or_404(Post, slug=slug)
+    if instance.draft or instance.publish > timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     share_string = quote_plus(instance.content)
 
     context = {
@@ -42,9 +45,12 @@ def post_detail(request, slug=None):
     return render(request, "post_detail.html", context)
 
 def post_list(request):
-    if not request.user.is_staff or not request.user.is_superuser:
-        raise Http404
-    queryset_list = Post.objects.all() #.order_by("-timestamp")
+    # if not request.user.is_staff or not request.user.is_superuser:
+    #     raise Http404
+    today = timezone.now().date
+    queryset_list = Post.objects.active() #.order_by("-timestamp")
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
     paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
     page_request_var = "page"
     page = request.GET.get(page_request_var)
@@ -61,7 +67,8 @@ def post_list(request):
     context = {
         "object_list": queryset,
         "title": "List",
-        "page_request_var": page_request_var
+        "page_request_var": page_request_var,
+        "today": today
     }
     return render(request, "post_list.html", context)
 
